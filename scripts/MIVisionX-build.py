0977cd5ca06e931ebc15000d6a3d7476d5a34835
kiriti <kiritigowda@gmail.com>
2018-12-11 10:28:34 -0800

    CMake options added

--- scripts/MIVisionX-build.py
+++ scripts/MIVisionX-build.py
@@ -12,25 +12,37 @@ import sys
 import subprocess
  
 
-opts, args = getopt.getopt(sys.argv[1:], 's:d:')
+opts, args = getopt.getopt(sys.argv[1:], 's:d:l:')
  
 sudoPassword = ''
 buildDir = ''
+linuxCMake = ''
 
 for opt, arg in opts:
     if opt == '-s':
         sudoPassword = arg
     elif opt == '-d':
     	buildDir = arg
+    elif opt =='-l':
+		linuxCMake = arg
 
 if sudoPassword == '':
-    print('Invalid command line arguments.\n \t\t\t\t-s [sudo password - required]\n \t\t\t\t-d [build directory - optional]\n ')
+    print('Invalid command line arguments.\n'\
+    	  ' \t\t\t\t-s [sudo password - required]\n'\
+    	  ' \t\t\t\t-l [Linux system cmake - optional (default:cmake options:cmake/cmake3)]\n '\
+    	  ' \t\t\t\t-d [build directory - optional]\n ')
     exit()
 
 if buildDir == '':
 	buildDir_MIVisionX = '~/MIVisionX'
 else:
 	buildDir_MIVisionX = buildDir+'MIVisionX'
+
+
+if linuxCMake == '' or linuxCMake == 'cmake':
+	linuxCMake = 'cmake'
+else:
+	linuxCMake = 'cmake3'
 
 # AMDOVX Work Flow
 buildMain_dir = os.path.expanduser(buildDir_MIVisionX)
@@ -52,7 +64,7 @@ if(os.path.exists(buildMake_dir)):
 else:
 	os.system('(cd '+buildMain_dir+'; mkdir build)');
 
-os.system('(cd '+buildMake_dir+'; cmake -DCMAKE_BUILD_TYPE=Release ../amdovx-modules )');
+os.system('(cd '+buildMake_dir+'; '+linuxCMake+' -DCMAKE_BUILD_TYPE=Release ../amdovx-modules )');
 os.system('(cd '+buildMake_dir+'; make -j8 )');
 from subprocess import call
 cmd='(cd '+buildMake_dir+'; sudo -S make install )'
